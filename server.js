4c878de8bd09f0531ce12d839e2f1f34559bf03a
oxyflour <a6905688@gmail.com>
2014-07-18 09:25:09 +0800

    host with phantomjs

--- server.js
+++ server.js
@@ -7,4 +7,14 @@ app.use('/share', express.static(__dirname+'/share'))
 	.use(express.static(__dirname+'/html'))
 srv.listen(18080)
 
+// start server
 new require('./share/game.js').Server(io)
+
+// start a phantomjs page as host
+require('phantom').create(function(ph) {
+	ph.createPage(function(page) {
+		page.open('http://localhost/?watch', function(status) {
+			console.log('hosting: ', status)
+		})
+	})
+})
